2fba81230e38472a323fbe2ce659e9df5638b65d
William Desportes <williamdes@wdes.fr>
2022-07-10 00:58:05 +0200

    Reach 100% code coverage by testing the ignore hops config

--- test/TlsIconTest.php
+++ test/TlsIconTest.php
@@ -3,8 +3,10 @@
 namespace RouncubeTlsIcon\Tests;
 
 require_once __DIR__ . '/rcube_plugin.php';
+require_once __DIR__ . '/rcmail.php';
 
 use tls_icon;
+use rcmail;
 use PHPUnit\Framework\TestCase;
 
 final class TlsIconTest extends TestCase
@@ -179,6 +181,84 @@ final class TlsIconTest extends TestCase
 				'others' => [
 					'received' => 'by aaa.bbb.ccc (Postfix, from userid 0)
 					id A70248414D5; Sun, 26 Apr 2020 16:49:01 +0200 (CEST)',
+				]
+			]
+		], $headersProcessed);
+	}
+
+	public function testMessageHeadersMultiFromWithConfig()
+	{
+		$inputHeaders = [
+			'from mail.example.org by mail.example.org with LMTP id pLzoBVClyGIiVgAA3BZZyA (envelope-from <bounces@bounces.example.org>) for <test@example.org>; Fri, 08 Jul 2022 21:44:48 +0000',
+			'from localhost (localhost [127.0.0.1]) by mail.example.org (Postfix) with ESMTP id 0D33249414 for <test@example.org>; Fri,  8 Jul 2022 21:44:48 +0000 (UTC)',
+			'from xxxx-ord.mtasv.net (xxxx-ord.mtasv.net [255.255.255.255]) (using TLSv1.2 with cipher ECDHE-RSA-AES256-GCM-SHA384 (256/256 bits)) (No client certificate requested) by mail.example.org (Postfix) with ESMTPS id 73C3B461AF for <test@example.fr>; Fri,  8 Jul 2022 21:44:39 +0000 (UTC)',
+			'by xxxx-ord.mtasv.net id hp2il427tk41 for <test@example.fr>; Fri, 8 Jul 2022 17:44:41 -0400 (envelope-from <bounces@bounces.example.org>)',
+		];
+
+		$o = new tls_icon();
+		$o->init();
+		rcmail::get_instance()->config->set('tls_icon_ignore_hops', 2);
+		$headersProcessed = $o->message_headers([
+			'output' => [
+				'subject' => [
+					'value' => 'Sent to you',
+				],
+			],
+			'headers' => (object) [
+				'others' => [
+					'received' => $inputHeaders,
+				]
+			]
+		]);
+		$this->assertEquals([
+			'output' => [
+				'subject' => [
+					'value' => 'Sent to you' . $this->strCryptedTlsv12WithCipher,
+					'html' => 1,
+				],
+			],
+			'headers' => (object) [
+				'others' => [
+					'received' => $inputHeaders,
+				]
+			]
+		], $headersProcessed);
+	}
+
+	public function testMessageHeadersMultiFromWithBadConfig()
+	{
+		$inputHeaders = [
+			'from mail.example.org by mail.example.org with LMTP id pLzoBVClyGIiVgAA3BZZyA (envelope-from <bounces@bounces.example.org>) for <test@example.org>; Fri, 08 Jul 2022 21:44:48 +0000',
+			'from localhost (localhost [127.0.0.1]) by mail.example.org (Postfix) with ESMTP id 0D33249414 for <test@example.org>; Fri,  8 Jul 2022 21:44:48 +0000 (UTC)',
+			'from xxxx-ord.mtasv.net (xxxx-ord.mtasv.net [255.255.255.255]) (using TLSv1.2 with cipher ECDHE-RSA-AES256-GCM-SHA384 (256/256 bits)) (No client certificate requested) by mail.example.org (Postfix) with ESMTPS id 73C3B461AF for <test@example.fr>; Fri,  8 Jul 2022 21:44:39 +0000 (UTC)',
+			'by xxxx-ord.mtasv.net id hp2il427tk41 for <test@example.fr>; Fri, 8 Jul 2022 17:44:41 -0400 (envelope-from <bounces@bounces.example.org>)',
+		];
+
+		$o = new tls_icon();
+		$o->init();
+		rcmail::get_instance()->config->set('tls_icon_ignore_hops', 1);
+		$headersProcessed = $o->message_headers([
+			'output' => [
+				'subject' => [
+					'value' => 'Sent to you',
+				],
+			],
+			'headers' => (object) [
+				'others' => [
+					'received' => $inputHeaders,
+				]
+			]
+		]);
+		$this->assertEquals([
+			'output' => [
+				'subject' => [
+					'value' => 'Sent to you' . $this->strUnEnCrypted,
+					'html' => 1,
+				],
+			],
+			'headers' => (object) [
+				'others' => [
+					'received' => $inputHeaders,
 				]
 			]
 		], $headersProcessed);
--- test/rcube_plugin.php
+++ test/rcube_plugin.php
@@ -10,4 +10,16 @@ class rcube_plugin
 		require_once __DIR__ . '/../localization/en_US.inc';
 		return $labels[$label];
 	}
+
+	public function load_config() {
+	}
+
+	public function add_hook($name, $params) {
+	}
+
+	public function include_stylesheet($name) {
+	}
+
+	public function add_texts($folderName) {
+	}
 }
