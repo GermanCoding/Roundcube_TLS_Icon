cea9956723f85ca8ea4419035d2fccb3528551ef
William Desportes <williamdes@wdes.fr>
2022-07-09 16:30:05 +0200

    Add a test for storage_init

--- test/TlsIconTest.php
+++ test/TlsIconTest.php
@@ -14,4 +14,22 @@ final class TlsIconTest extends TestCase
 		$o = new tls_icon();
 		$this->assertInstanceOf('tls_icon', $o);
 	}
+
+	public function testStorage_Init()
+	{
+		$o = new tls_icon();
+		$this->assertSame([
+			'fetch_headers' => ' RECEIVED'
+		], $o->storage_init([]));
+		$this->assertSame([
+			'fetch_headers' => ' RECEIVED'
+		], $o->storage_init(['fetch_headers' => null]));
+		$this->assertSame([
+			'fetch_headers' => 'foo bar RECEIVED'
+		], $o->storage_init(['fetch_headers' => 'foo bar']));
+		$this->assertSame([
+			'fetch_headers' => 'spaces RECEIVED'
+		], $o->storage_init(['fetch_headers' => 'spaces   ']));
+	}
+
 }
