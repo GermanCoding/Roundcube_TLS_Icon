4466adf46ff5825827643323ae5273160c3d5b9a
dehoej <110595168+dehoej@users.noreply.github.com>
2024-09-16 21:39:08 +0200

    Update tls_icon.php

--- tls_icon.php
+++ tls_icon.php
@@ -5,7 +5,7 @@ class tls_icon extends rcube_plugin
 	const POSTFIX_TLS_REGEX = "/\(using (TLS.*)\) \(/im";
 	const POSTFIX_LOCAL_REGEX = "/\([a-zA-Z]*, from userid [0-9]*\)/im";
 	const SENDMAIL_TLS_REGEX = "/\(version=(TLS.*)\)(\s+for|;)/im";
-	const STALWART_TLS_REGEX = "/\(using TLSv1\.[23] with cipher .+\)/im";
+    const STALWART_TLS_REGEX = "/\(using (TLSv1\.[23] with cipher .+)\) by .+ \(Stalwart SMTP\)/im";
 
 	private $message_headers_done = false;
 	private $icon_img;
